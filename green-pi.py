ca5ffaea7040c398146358a864fcf234ae1e7228
Timur <timur@xigit.de>
2020-06-09 14:34:49 +0100

    feat: added humidty reading

--- green-pi.py
+++ green-pi.py
@@ -9,19 +9,19 @@ from time import strftime
 import Adafruit_DHT as dht
 import random
 
-from db import add_sensor_data
+#from db import add_sensor_data
 
 __author__ = "Timur Yigit"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-
+'''
 def job():
     add_sensor_data({
         'air_temp': round(random.uniform(1.0, 100.0), 2),
         'humidity': round(random.uniform(1.0, 100.0), 2),
         'moister': round(random.uniform(1.0, 100.0), 2)})
-
+'''
 
 # Get list all sensor GPIO pins stored in DB
 def fetchSensorGPIO():
@@ -39,8 +39,8 @@ def getGrowData():
     data = {}
     data['timestamp'] = strftime("%Y-%m-%d %H:%M:%S")
     data['temperature'] = fetchRawTemperature(GPIO['climate_GPIO'])
-    '''
     data['humidity'] = fetchRawHumidity(GPIO['climate_GPIO'])
+    '''
     data['light_status'] = getGPIOState(GPIO['light_GPIO'])
     data['moisture_status'] = getGPIOState(GPIO['moisture_GPIO'])
     data['fan_status'] = getGPIOState(GPIO['fan_GPIO'])
@@ -61,6 +61,20 @@ def fetchRawTemperature(gpioPIN):
 
         if temperature is not None:
             data_output = round(temperature, 2)
+            print(data_output)
+            return data_output
+        else:
+            print('Failed to get reading. Try again!')
+    except:
+        print("Sensor Error!")
+
+# Fetch Raw Humidity
+def fetchRawHumidity(gpioPIN):
+    try:
+        humidity,temperature = dht.read_retry(dht.DHT22, int(gpioPIN))
+
+        if humidity is not None and humidity <= 100:
+            data_output = round(humidity, 2)
             print(data_output)
             return data_output
         else:

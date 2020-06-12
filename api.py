eb5ff5276a63b8b268c3d1c2c4f8176617669c65
Abed Hawa <abed@Abeds-MBP.fritz.box>
2020-06-12 15:16:51 +0200

    fix code stype

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from db import get_schedules, update_schedule, enable_schedule, disabe_schedule,
 from marshmallow import Schema, fields
 import logging
 
+
 class ScheduleSchema(Schema):
     id = fields.Int()
     start_schedule = fields.Time()
@@ -11,7 +12,9 @@ class ScheduleSchema(Schema):
     manual_schedule = fields.Bool()
     device_id = fields.Int()
 
+
 app = Flask(__name__)
+
 
 @app.route('/schedules', methods=['GET'])
 def schedules():
@@ -19,11 +22,12 @@ def schedules():
     schedules = get_schedules(True)
     return jsonify(schema.dump(schedules))
 
+
 @app.route('/schedules', methods=['POST'])
 def schedules_add():
     try:
         data = request.get_json()
-    except:
+    except BaseException:
         logging.error("Error reading the request body")
         return jsonify({"message": "invalid request"})
     add_schedule(
@@ -32,35 +36,38 @@ def schedules_add():
         enabled=1,
         device_id=data.get('device_id'),
         manual_schedule=1
-        )
+    )
     return jsonify({"message": "schedule was added successfully"})
+
 
 @app.route('/schedules/<int:schedule_id>', methods=['PUT'])
 def schedule_update(schedule_id):
     try:
         data = request.get_json()
-    except:
+    except BaseException:
         logging.error("Error reading the request body")
         return jsonify({"message": "invalid request"})
-        
     update_schedule(
         schedule_id,
         start_schedule=data.get('start_schedule'),
         end_schedule=data.get('end_schedule'),
         device_id=data.get('device_id')
-        )
+    )
 
     return jsonify({"message": "schedule was updated successfully"})
+
 
 @app.route('/schedules/<int:schedule_id>', methods=['DELETE'])
 def schedule_delete(schedule_id):
     delete_schedule(schedule_id)
     return jsonify({"message": "schedule was deleted successfully"})
 
+
 @app.route('/schedules/<int:schedule_id>/enable', methods=['PUT'])
 def schedule_enable(schedule_id):
     enable_schedule(schedule_id)
     return jsonify({"message": "schedule was enabled successfully"})
+
 
 @app.route('/schedules/<int:schedule_id>/disable', methods=['PUT'])
 def schedule_disable(schedule_id):
